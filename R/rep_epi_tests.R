46328c1f139e0af8412093d058a56e498eab1782
agdamsbo <agdamsbo@protonmail.com>
2018-10-22 14:05:19 +0200

    update

--- R/rep_epi_tests.R
+++ R/rep_epi_tests.R
@@ -1,24 +1,40 @@
 #' A repeated epi.tests function
 #'
-#' Repeats the epi.tests from the epiR package.
+#' Repeats the epi.tests from the epiR package. Either gs or test should be of length 1.
 #' @description For bivariate analyses. The confint() function is rather slow, causing the whole function to hang when including many predictors and calculating the ORs with CI.
-#' @param gs the test or meassure used as "golden standard". Format as dichotomised factor.
+#' @param gold the test or meassure used as "golden standard". Format as dichotomised factor.
 #' @param test possible predictive tests to evaluate. Format as dichotomised factor.
+#' @param data dataframe to draw variables from.
 #' @keywords ppv npv sensitivity specificity
 #' @export
 #' @examples
 #' rep_epi_tests()
 
 
-rep_epi_tests<-function(gs,test){
+rep_epi_tests<-function(gold,test,data){
   require(epiR)
 
+  d<-data
+  test<-d[,c(test)]
+
+  gs<-d[,c(gold)]
+
   ls<-list()
+
+if (length(gold)==1){
   for (i in 1:ncol(test)){
     t<-table(test[,i],gs)
     rval <- epi.tests(t, conf.level = 0.95)
     n<-names(test)[i]
     ls[[i]]<-list(n,rval)
+  }}
+else {
+  for (i in 1:ncol(gs)){
+    t<-table(test,gs[,i])
+    rval <- epi.gss(t, conf.level = 0.95)
+    n<-names(gs)[i]
+    ls[[i]]<-list(n,rval)
   }
+}
   return(ls)
 }
