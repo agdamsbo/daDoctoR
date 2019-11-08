84e390f378dad632d5b9384e19aaad4c6e00d5a0
agdamsbo <agdamsbo@protonmail.com>
2019-11-08 10:59:45 +0100

    updates
    now with a shiny hwe-app

---
+++ R/hwe_app.R
@@ -0,0 +1,13 @@
+#' Shiny app of the Hardy Weinberg Equillibrium calculations
+#'
+#' App to easily calculate and visualize the HWE.
+#'
+#' @export
+hwe_app <- function() {
+  appDir <- system.file("shiny-examples", "hwe_calc", package = "daDoctoR")
+  if (appDir == "") {
+    stop("Could not find app directory. Try re-installing `daDoctoR`.", call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
